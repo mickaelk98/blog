da1e69f8300cb34b0e28f7b2439cc8682be6dac0
mickael keita <keitamickael@gmail.com>
2024-10-08 14:56:56 +0200

    affichage des articles sur la page d'accueil

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
+<?php
+
+$filename = __DIR__ . "\data\articles.json";
+$articles = [];
+$categories = [];
+
+if (file_exists($filename)) {
+    $articles = json_decode(file_get_contents($filename), true) ?? [];
+    $cattmp = array_map(fn($article) => $article["category"], $articles);
+    $categories = array_reduce($cattmp, function ($acc, $cat) {
+        if (isset($acc[$cat])) {
+            $acc[$cat]++;
+        } else {
+            $acc[$cat] = 1;
+        }
+        return $acc;
+    }, []);
+    $articlePerCategories = array_reduce($articles, function ($acc, $article) {
+        if (isset($acc[$article["category"]])) {
+            $acc[$article["category"]] = [...$acc[$article["category"]], $article];
+        } else {
+            $acc[$article["category"]] = [$article];
+        }
+
+        return $acc;
+    }, []);
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -8,7 +38,23 @@
 
 <body class="flex flex-col h-screen font-poppins bg-slate-100">
     <?php require_once "includes/header.php" ?>
-    <main class="flex-1"></main>
+    <main class="flex-1 flex flex-col self-center items-center py-8 max-w-5xl">
+        <div>
+            <?php foreach ($categories as $cat => $num): ?>
+                <h1 class="text-xl font-bold px-2"><?= $cat ?></h1>
+                <div class="flex flex-row flex-wrap justify-center items-start mb-5">
+                    <?php foreach ($articlePerCategories[$cat] as $article): ?>
+                        <article class="flex-[30%] m-2 bg-light shadow-md min-h-[365px] cursor-pointer">
+                            <div class="overflow-hidden">
+                                <div class=" bg-[url('<?= $article["image"] ?>')] h-[300px] bg-cover hover:scale-125 transition duration-500"></div>
+                            </div>
+                            <h2 class="p-2"><?= $article["title"] ?></h2>
+                        </article>
+                    <?php endforeach; ?>
+                </div>
+            <?php endforeach ?>
+        </div>
+    </main>
     <?php require_once "includes/footer.php" ?>
 </body>
 
